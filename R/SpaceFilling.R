f3091942edfe7907f21c01c41eb8bb6eaa952e60
Mohamed Laib <laib.med@gmail.com>
2017-05-16 11:24:14 +0200

    Update SpaceFilling.R

--- R/SpaceFilling.R
+++ R/SpaceFilling.R
@@ -1,72 +1,110 @@
-SpaceFilling <-
-function(asch){
+SpaceFilling <-function(asch){
+  fun1<-function() {
+    n<-readline("Number of lines of association schemes array :\n")
+    l<-readline("Number of columns of association schemes array :\n")
+    n<-as.integer(n);l<-as.integer(l)
+    return(c(n,l))
+  }
 
-fun1<-function() {
-n<-readline("Number of lines of association schemes array :\n")
-l<-readline("Number of columns of association schemes array :\n")
-n<-as.integer(n);l<-as.integer(l)
-return(c(n,l))}
+  fun2<-function() {
+    n<-readline("Number of lines of association schemes array :\n")
+    l<-readline("Number of columns of association schemes array :\n")
+    w<-readline("Number of the association scheme arrays :\n")
+    n<-as.integer(n);l<-as.integer(l);w<-as.integer(w)
+  return(c(n,l,w))}
 
-fun2<-function() {
-n<-readline("Number of lines of association schemes array :\n")
-l<-readline("Number of columns of association schemes array :\n")
-w<-readline("Number of the association scheme arrays :\n")
-n<-as.integer(n);l<-as.integer(l);w<-as.integer(w)
-return(c(n,l,w))}
-
-# Si Div
-if (asch == "Div"){
-V<-fun1();n<-V[1];l<-V[2]
-s<-n*l;A<-matrix(1:s, ncol = V[2], byrow=TRUE)
-SF<-matrix(ncol=s,nrow=s)
-for (d in 1:s) {
-SF[d,d]<-1
-for (dd in 1:s){
-D<-which(A==d); d1<-D%%n ; if (d1==0){d1<-n};DD<-which(A==dd); d2<-DD%%n ; if (d2==0){d2<-n}
-if (d1==d2) {SF[d,dd]<-1;SF[dd,d]<-1}
-else {SF[d,dd]<-2;SF[dd,d]<-2}}}}
+# If Div
+  if (asch == "Div"){
+    V<-fun1();n<-V[1];l<-V[2]
+    s<-n*l;A<-matrix(1:s, ncol = V[2], byrow=TRUE)
+    SF<-matrix(ncol=s,nrow=s)
+    for (d in 1:s) {
+      SF[d,d]<-1
+      for (dd in 1:s){
+        D<-which(A==d)
+        d1<-D%%n 
+        if (d1==0){d1<-n};DD<-which(A==dd)
+        d2<-DD%%n
+        if (d2==0){d2<-n}
+        if (d1==d2) {SF[d,dd]<-1;SF[dd,d]<-1}
+        else {SF[d,dd]<-2;SF[dd,d]<-2}
+      }
+    }
+  }
 
 ##SI Rect
-if (asch == "Rect"){
-V<-fun1();n<-V[1];l<-V[2];s<-n*l;A<-matrix(1:s, ncol =l, byrow=TRUE)
-SF<-matrix(ncol=s,nrow=s)
-for (d in 1:s) {
-SF[d,d]<-1
-for (dd in 1:s){
-B<-t(A)
-D<-which(A==d); d1<-D%%n ; if (d1==0){d1<-n};DD<-which(A==dd); d2<-DD%%n 
-if (d2==0){d2<-n}
-D1<-which(B==d); d11<-D1%%l ; if (d11==0){d11<-l};DD1<-which(B==dd); d12<-DD1%%l
-if (d12==0){d12<-l}
-if (d1==d2) {SF[d,dd]<-1;SF[dd,d]<-1}
-if (is.na(SF[d,dd])==TRUE){
-if (d11==d12) {SF[d,dd]<-2;SF[dd,d]<-2}}}}
-for (d in 1:s) {
-for (dd in 1:s){
-if (is.na(SF[d,dd])==TRUE){
-SF[d,dd]<-3;SF[dd,d]<-3}}}}
+  if (asch == "Rect"){
+    V<-fun1();n<-V[1];l<-V[2];s<-n*l;A<-matrix(1:s, ncol =l, byrow=TRUE)
+    SF<-matrix(ncol=s,nrow=s)
+    for (d in 1:s) {
+      SF[d,d]<-1
+      for (dd in 1:s){
+        B<-t(A)
+        D<-which(A==d)
+        d1<-D%%n 
+        if (d1==0){d1<-n}
+        DD<-which(A==dd)
+        d2<-DD%%n 
+        if (d2==0){d2<-n}
+        D1<-which(B==d)
+        d11<-D1%%l
+        if (d11==0){d11<-l}
+        DD1<-which(B==dd)
+        d12<-DD1%%l
+        if (d12==0){d12<-l}
+        if (d1==d2) {SF[d,dd]<-1;SF[dd,d]<-1}
+        if (is.na(SF[d,dd])==TRUE){
+        if (d11==d12) {SF[d,dd]<-2;SF[dd,d]<-2}
+        }
+      }
+    }
+       
+    for (d in 1:s) {
+      for (dd in 1:s){
+        if (is.na(SF[d,dd])==TRUE){
+          SF[d,dd]<-3;SF[dd,d]<-3
+        }
+      }
+    }
+  }
 
 ##si Nestdiv 
-if (asch == "Nestdiv"){
-V<-fun2();n<-V[1];l<-V[2];w<-V[3]
-s<-l*n;A<-NULL;S<-l*n*w
-SF<-matrix(ncol=S,nrow=S)
-for (i in 1:w){
-A[[i]]<-matrix(1:s, ncol=l, byrow=TRUE)
-z<-(i-1)*s
-A[[i]]<-A[[i]]+z};B<-Reduce("rbind",A)
-for (i in 1:w) {
-a<-A[[i]];mi<-min(a);ma<-max(a)
-for (d in mi:ma){
-for (dd in mi:ma){
-D<-which(a==d); d1<-D%%n ; if (d1==0){d1<-n}
-DD<-which(a==dd); d2<-DD%%n ; if (d2==0){d2<-n}
-if (d1==d2) {SF[d,dd]<-1;SF[dd,d]<-1}
-else {SF[d,dd]<-2;SF[dd,d]<-2}}}}
-for (d in 1:S) {
-for (dd in 1:S){
-if (is.na(SF[d,dd])==TRUE){
-SF[d,dd]<-3;SF[dd,d]<-3}}}}
+  if (asch == "Nestdiv"){
+    V<-fun2();n<-V[1];l<-V[2];w<-V[3]
+    s<-l*n
+    A<-NULL
+    S<-l*n*w
+    SF<-matrix(ncol=S,nrow=S)
+    for (i in 1:w){
+      A[[i]]<-matrix(1:s, ncol=l, byrow=TRUE)
+      z<-(i-1)*s
+      A[[i]]<-A[[i]]+z
+    }
+    B<-Reduce("rbind",A)
+    for (i in 1:w) {
+      a<-A[[i]];mi<-min(a);ma<-max(a)
+      for (d in mi:ma){
+        for (dd in mi:ma){
+          D<-which(a==d)
+          d1<-D%%n 
+          if (d1==0){d1<-n}
+          DD<-which(a==dd)
+          d2<-DD%%n
+          if (d2==0){d2<-n}
+          if (d1==d2) {SF[d,dd]<-1;SF[dd,d]<-1}
+          else {SF[d,dd]<-2;SF[dd,d]<-2}
+        }
+      }
+    }
+  for (d in 1:S) {
+    for (dd in 1:S){
+      if (is.na(SF[d,dd])==TRUE){
+      SF[d,dd]<-3
+      SF[dd,d]<-3
+      }
+    }
+  }
+ }
 
 
 #### SI RightAng
